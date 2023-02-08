e268259a9b7230b670816f18a9d813f6928cd27a
Mdmosley12 <mdmosley12@gmail.com>
2023-02-08 17:04:52 +0000

    add seeMore feature

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react"
 import { getArticles } from "../utils/api"
 import { Link } from 'react-router-dom'
 import { Nav } from "./Nav"
+import { truncateString } from "../utils/truncateString"
 
 export const Articles = () => {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
+    const [limit, setLimit] = useState(8);
 
     useEffect(() => {
         getArticles().then((articlesFromApi) => {
@@ -18,20 +20,25 @@ export const Articles = () => {
         return <h2 id="loading">Loading...</h2>
     }
 
+    const handleSeeMore = () => {
+        setLimit(articles.length);
+      };
+    const handleSeeLess = () => {
+        setLimit(8)
+    }
+
     return (
         <section id="articles">
             <Nav setArticles={setArticles}/>
             <ul>
-                {articles.map((article) => {
+                {articles.slice(0, limit).map((article) => {
                     return <li key={article.article_id}>
                         <Link to={`/articles/${article.article_id}`}>
                             <div className="articleContainer"><br/>
-                            <p id="articleTitle">{article.title}</p>
-                            <br/>
                             Posted by {article.author}&nbsp;
                             in {article.topic}<br />
                             <img  className="articleImg" alt="relevant to article" src={article.article_img_url}></img>
-                            <br/>
+                            <p id="articleTitle">{truncateString(article.title)}</p>
                             Comments: {article.comment_count}
                             &nbsp;
                             Votes: {article.votes}
@@ -39,6 +46,8 @@ export const Articles = () => {
                         </Link>
                     </li>
                 })}
+                {limit < articles.length ? <button id="seeMoreButton" onClick={handleSeeMore}>See More...</button> : null}
+                {limit > 8 ? <button id="seeLessButton" onClick={handleSeeLess}>See Less...</button> : null}
             </ul>
         </section>
     )
