421e7f78483a9239c850d8f8c21ca1740efcdb59
Mdmosley12 <mdmosley12@gmail.com>
2023-02-09 09:22:19 +0000

    add popular articles function

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,6 +3,7 @@ import { getArticles } from "../utils/api"
 import { Link } from 'react-router-dom'
 import { Nav } from "./Nav"
 import { truncateString } from "../utils/truncateString"
+import { PopularArticles } from "./PopularArticles"
 
 export const Articles = () => {
     const [articles, setArticles] = useState([])
@@ -30,11 +31,15 @@ export const Articles = () => {
     return (
         <section id="articles">
             <Nav setArticles={setArticles}/>
+            <div id="articlesBody">
+            <h2 id="popularArticlesHeader">Popular Articles</h2>
+            <PopularArticles  articles={articles}/>
+            <h2 id="allArticlesHeader">All Articles</h2>
             <ul>
                 {articles.slice(0, limit).map((article) => {
                     return <li key={article.article_id}>
                         <Link to={`/articles/${article.article_id}`}>
-                            <div className="articleContainer"><br/>
+                            <div className="articleContainer">
                             Posted by {article.author}&nbsp;
                             in {article.topic}<br />
                             <img  className="articleImg" alt="relevant to article" src={article.article_img_url}></img>
@@ -49,6 +54,7 @@ export const Articles = () => {
                 {limit < articles.length ? <button id="seeMoreButton" onClick={handleSeeMore}>See More...</button> : null}
                 {limit > 8 ? <button id="seeLessButton" onClick={handleSeeLess}>See Less...</button> : null}
             </ul>
+            </div>
         </section>
     )
 }
